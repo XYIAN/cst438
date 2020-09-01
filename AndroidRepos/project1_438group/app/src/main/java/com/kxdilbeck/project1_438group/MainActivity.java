b7f90698588770e865331104cecbc91de1dff2c6
XYIAN <kylexyiandilbeck@gmail.com>
2020-09-01 16:40:38 -0700

    update

---
+++ AndroidRepos/project1_438group/app/src/main/java/com/kxdilbeck/project1_438group/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kxdilbeck.project1_438group;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
